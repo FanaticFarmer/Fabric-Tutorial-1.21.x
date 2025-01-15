e2b6a07531cd532e196872d323dbba86f91f68a3
FanaticFarmer <ffarmercontacto@gmail.com>
2025-01-15 18:19:48 +0100

    Fabric Modding Tutorial 1.21 #2 Custom Items Started and finished

--- src/main/java/net/fanaticfarmer/tutorialmod/TutorialMod.java
+++ src/main/java/net/fanaticfarmer/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.fanaticfarmer.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fanaticfarmer.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,8 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+
+		ModItems.registerModItems();
 
 	}
 }
