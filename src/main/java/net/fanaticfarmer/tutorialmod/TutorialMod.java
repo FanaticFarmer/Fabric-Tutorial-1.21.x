cbe033b152a75d15ee4d4392ae65630ed9f114e8
FanaticFarmer <ffarmercontacto@gmail.com>
2025-01-15 19:07:58 +0100

    Fabric Modding Tutorial 1.21 #4 Item Groups Started and finished

--- src/main/java/net/fanaticfarmer/tutorialmod/TutorialMod.java
+++ src/main/java/net/fanaticfarmer/tutorialmod/TutorialMod.java
@@ -3,6 +3,7 @@ package net.fanaticfarmer.tutorialmod;
 import net.fabricmc.api.ModInitializer;
 
 import net.fanaticfarmer.tutorialmod.block.ModBlocks;
+import net.fanaticfarmer.tutorialmod.item.ModItemGroups;
 import net.fanaticfarmer.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,8 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+
+		ModItemGroups.registerItemGroups();
 
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
