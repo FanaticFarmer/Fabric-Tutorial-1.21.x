fa6bde52075706a3b07ba4d8d3129e74e9696675
FanaticFarmer <ffarmercontacto@gmail.com>
2025-01-15 17:28:53 +0100

    yummers added

--- src/main/java/net/fanaticfarmer/tutorialmod/TutorialMod.java
+++ src/main/java/net/fanaticfarmer/tutorialmod/TutorialMod.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
+	//yummers
 	@Override
 	public void onInitialize() {
 
